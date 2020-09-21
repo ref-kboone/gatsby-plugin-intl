b5a5b43e86e580e639eecf1c1cfa3b72bfaf5c44
Hannes Vermeire <hvermeire@reference.be>
2020-09-21 10:22:38 +0200

    Re-do the normalize function to also include leading double slashes

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,13 +1,7 @@
 const webpack = require("webpack")
 
 function normalize(url) {
-  const set = url.match(/([^:]\/{2,3})/g) // Match (NOT ":") followed by (2 OR 3 "/")
-
-  for (const str in set) {
-    const replace_with = set[str].substr(0, 1) + "/"
-    url = url.replace(set[str], replace_with)
-  }
-  return url
+  return url.replace(/(https?:\/\/)|(\/)+/g, "$1$2")
 }
 
 function flattenMessages(nestedMessages, prefix = "") {
--- src/link.js
+++ src/link.js
@@ -71,11 +71,5 @@ export const changeLocale = (language, to) => {
 }
 
 const normalize = url => {
-  const set = url.match(/([^:]\/{2,3})/g) // Match (NOT ":") followed by (2 OR 3 "/")
-
-  for (const str in set) {
-    const replace_with = set[str].substr(0, 1) + "/"
-    url = url.replace(set[str], replace_with)
-  }
-  return url
+  return url.replace(/(https?:\/\/)|(\/)+/g, "$1$2")
 }
